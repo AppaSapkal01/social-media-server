37527367dea60261750d496b9ef6503887b3bd0f
AppaSapkal01 <appasapkal26977@gmail.com>
2023-07-24 21:07:55 +0530

    created PostController, postModel, PostRoute for Post functionality i.e Creating Post and getting post

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import AuthRoute from './Routes/AuthRoute.js';
-import UserRoute from './Routes/UserRoute.js'
+import UserRoute from './Routes/UserRoute.js';
+import PostRoute from './Routes/PostRoute.js';
 
 // Routes
 
@@ -30,6 +31,7 @@ mongoose
     // usage of routes
     app.use('/auth', AuthRoute);
     app.use('/user', UserRoute);
+    app.use('/post', PostRoute);
 
 
 
